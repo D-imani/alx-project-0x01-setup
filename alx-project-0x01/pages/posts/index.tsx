ff69efec8982e76c54378fb12a5d480e5ef1a684
Dorna <dorna.imani943@gmail.com>
2025-07-20 22:52:10 +0300

    Navigation Set up

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import PostCard from "@/components/common/PostCard";
+import Header from "@/components/layout/Header";
 
 const PostsPage: React.FC = () => {
   return (
     <div className="p-6">
+    <Header />
       <h1 className="text-2xl font-semibold mb-4">Posts</h1>
       <PostCard title="Sample Post" description="This is a placeholder for a post." />
     </div>
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 
 const UsersPage: React.FC = () => {
   return (
     <div className="p-6">
+        <Header />
       <h1 className="text-2xl font-semibold">Users</h1>
       <p>This is a placeholder for the users page.</p>
     </div>
