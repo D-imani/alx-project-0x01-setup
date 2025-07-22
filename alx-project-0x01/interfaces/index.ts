0e53e42b437337ff7b88d2e8cff807dd811c5b1c
Dorna <dorna.imani943@gmail.com>
2025-07-22 15:04:40 +0300

    indesx.tsx interface-01

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,8 +1,8 @@
 export interface PostProps {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+  userId: number,
+  id: number,
+  title: string,
+  body: string,
 }
 export interface UserProps {
     id : number ,
